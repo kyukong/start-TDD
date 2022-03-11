c531eb6a517e0c14edd5550180bb19d6d0619e9a
yukong <kyb062666@gmail.com>
2022-03-12 00:28:01 +0900

    ch02(암호 검사기): 숫자를 포함하지 않으며 다른 조건은 만족하는 경우
    - 테스트 코드 및 프로덕션 코드 리팩토링

--- src/main/java/ch02/PasswordStrengthMeter.java
+++ src/main/java/ch02/PasswordStrengthMeter.java
@@ -5,9 +5,22 @@ public class PasswordStrengthMeter {
     private static final int PASSWORD_LENGTH_MINIMUM = 8;
 
     public PasswordStrength meter(final String password) {
-        if (password.length() < PASSWORD_LENGTH_MINIMUM) {
+        if (isUnderValidLength(password) || !(isContainsNumber(password))) {
             return PasswordStrength.NORMAL;
         }
         return PasswordStrength.STRONG;
+    }
+
+    private boolean isUnderValidLength(final String password) {
+        return password.length() < PASSWORD_LENGTH_MINIMUM;
+    }
+
+    private boolean isContainsNumber(final String password) {
+        for (String text : password.split("")) {
+            if (text.matches("[0-9]+")) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/test/java/ch02/PasswordStrengthMeterTest.java
+++ src/test/java/ch02/PasswordStrengthMeterTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PasswordStrengthMeterTest {
 
+    private final PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
     /**
      * TDD 예: 암호 검사기
      *
@@ -22,22 +24,20 @@ public class PasswordStrengthMeterTest {
     @DisplayName("모든 조건을 만족하는 경우")
     @Test
     void strong() {
-        PasswordStrengthMeter passwordStrengthMeter = new PasswordStrengthMeter();
-        PasswordStrength meter = passwordStrengthMeter.meter("1234ABCD");
-
-        assertEquals(meter, PasswordStrength.STRONG);
+        assertEquals(meter.meter("1234ABCD"), PasswordStrength.STRONG);
     }
 
     @DisplayName("길이가 8 글자 미만이며 다른 조건은 만족하는 경우")
     @Test
     void normal_length_under_8() {
-        PasswordStrengthMeter passwordStrengthMeter = new PasswordStrengthMeter();
-        PasswordStrength meter = passwordStrengthMeter.meter("123ABCD");
-
-        assertEquals(meter, PasswordStrength.NORMAL);
+        assertEquals(meter.meter("123ABCD"), PasswordStrength.NORMAL);
     }
 
-    // TODO: 0 부터 9 사이의 숫자를 포함하지 않는 경우 -> 보통
+    @DisplayName("숫자를 포함하지 않으며 다른 조건은 만족하는 경우")
+    @Test
+    void normal_not_contains_number() {
+        assertEquals(meter.meter("ABCDEFGH"), PasswordStrength.NORMAL);
+    }
 
     // TODO: 대문자를 포함하지 않는 경우 -> 보통
 
